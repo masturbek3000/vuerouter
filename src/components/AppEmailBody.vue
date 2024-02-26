794164f591fe46c8d62b3eb1e8063cd8d654baf9
masturbek3000 <fantasyhero777@gmail.com>
2024-02-26 16:35:30 +0500

    fixed emailbody

--- src/components/AppEmailBody.vue
+++ src/components/AppEmailBody.vue
@@ -1,26 +1,28 @@
 <template>
-    <div v-if="email">
-      <h2>{{ email.theme }}</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi, cumque ea laborum laudantium nostrum odio quis soluta sunt velit?</p>
-      <button class="btn" @click="$router.push('/mail')">Закрыть</button>
-    </div>
-    <div v-else>
-        <h4>Выберите письмо</h4>
-    </div>
-  </template>
+  <div v-if="email">
+    <h2>{{ email.theme }}</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi,
+      cumque ea laborum laudantium nostrum odio quis soluta sunt velit?
+    </p>
+    <button class="btn" @click="$router.push('/mail')">Закрыть</button>
+  </div>
+  <div v-else>
+    <h4>Выберите письмо</h4>
+  </div>
+</template>
   
   <script>
-  export default {
-    inject: ['emails'],
-    props: ['mailId'],
-    computed: {
-        email() {
-            return this.emails.find(e => e.id === this.mailId)
-        }
-    }
-  }
-  </script>
+export default {
+  inject: ["emails"],
+  props: ["mailId"],
+  computed: {
+    email() {
+      return this.emails.find((e) => e.id == this.mailId);
+    },
+  },
+};
+</script>
   
   <style scoped>
-  
-  </style>
+</style>
